9400abe8d16ef2a8481fa8dc56a2e06a396682dd
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-22 12:51:08 -0600

    Create List component

---
+++ src/js/components/List.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const mapStateToProps = state => {
+  return { articles: state.articles };
+};
+
+// List component receives prop articles from the reducer
+const ConnectedList = ({ articles }) => (
+  <ul>
+    {articles.map(el => (
+      <li key={el.id}>{el.title}</li>
+    ))}
+  </ul>
+)
+
+// connect is MAJOR KEY 🔑 for connecting react component with redux
+// connect takes at least one argument
+const List = connect(mapStateToProps)(ConnectedList);
+
+export default List;
