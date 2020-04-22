1fa6c87761dab2f02bea24b9f30084906deb4e02
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-22 11:44:27 -0600

    Update reducer with if statement for checking action type

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,10 +1,19 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
 const initialstate = {
   articles: []
 };
 
 // initial state passed as a default parameter
 function rootReducer(state = initialstate, action) {
+  if (action.type === ADD_ARTICLE) {
+    // use Object.assign to return a new object (dont wanna alter og state)
+    return Object.assign({}, state, {
+      // use concat (instead of push) for keeping og array
+      articles: state.articles.concat(action.paylaod)
+    })
+  }
   return state;
-};
+}
 
 export default rootReducer;
