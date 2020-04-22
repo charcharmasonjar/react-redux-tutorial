56e2210be1b5c8a8958998821c59413c97da77bf
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-22 11:32:15 -0600

    Create simple reducer

---
+++ src/js/reducers/index.js
@@ -0,0 +1,10 @@
+const initialstate = {
+  articles: []
+};
+
+// initial state passed as a default parameter
+function rootReducer(state = initialstate, action) {
+  return state;
+};
+
+export default rootReducer;
