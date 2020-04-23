e939eb1d8fdbadcbcc8e1509d6b4e77eef78dd2f
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-23 11:32:31 -0600

    Fix App component import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index";
-import App from "./App";
+import App from "./js/components/App";
 
+// provider wraps up reac app and makes it aware
+// of the entire redux's store
 render (
   <Provider store={store}>
     <App />
