4e37124dbabf83a92fd2fe853500b3fc503bd055
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-23 11:32:17 -0600

    Add Form component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import List from "./List";
+import Form from "./Form";
 
 const App = () => (
   <div>
     <h2>Articles</h2>
     <List />
+    <div>
+      <h2>Add a new article</h2>
+      <Form />
+    </div>
   </div>
 )
 
