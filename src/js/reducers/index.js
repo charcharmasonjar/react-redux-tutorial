a3a48fb369179b6a0c226095d6c791a50b2da4cf
charcharmasonjar <charlotte.dibb@gmail.com>
2020-04-23 11:59:20 -0600

    FIX TYPO

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,17 +1,14 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 
-const initialstate = {
+const initialState = {
   articles: []
 };
 
-// initial state passed as a default parameter
-function rootReducer(state = initialstate, action) {
+function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
-    // use Object.assign to return a new object (dont wanna alter og state)
     return Object.assign({}, state, {
-      // use concat (instead of push) for keeping og array
-      articles: state.articles.concat(action.paylaod)
-    })
+      articles: state.articles.concat(action.payload)
+    });
   }
   return state;
 }
